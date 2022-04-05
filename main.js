415e5ee9ad78e3724e873589cfb9a5711c455a54
Zach Nelson <zen123555@gmail.com>
2022-04-04 17:58:39 -0700

    Add basic JS functionality

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
+
+let rock = document.querySelector('#rock');
+let paper = document.querySelector('#paper');
+let scissors = document.querySelector('#scissors');
+
+let scoreboard = document.querySelector('#scoreboard');;
+
+let playerWinTotal = 0;
+let compWinTotal = 0;
+let tieTotal = 0;
+
+//configuring elements for results output
+let resultsContainer = document.querySelector('#results-container');
+const resultsContent = document.createElement('p');
+resultsContent.classList.add('results-content');
+
+rock.addEventListener('click', () => playRound('rock'));
+paper.addEventListener('click', () => playRound('paper'));
+scissors.addEventListener('click', () => playRound('scissors'));
+
 function computerPlay(){
     let result;
-    switch (Math.floor(Math.random()*(3-1+1)) + 1) {
+    switch (Math.floor(Math.random()*(3-1+1)) + 1){ //gets random number for comp selection 
         case 1:
             result = 'scissors'
             break;
@@ -11,63 +31,111 @@ function computerPlay(){
             result = 'rock';
             break;
         default:
-            console.log('default case reached, not correct behavior.');
+            console.log('default case reached, not correct behavior in computerPlay function.');
     }
     return result;
 }
 
-let playerWinTotal = 0;
-let compWinTotal = 0;
-let tieTotal = 0;
+function playRound(playerSelection, computerSelection = computerPlay()){
 
-function playRound(playerSelection = window.prompt('Enter rock, paper, or scissors'), computerSelection = computerPlay()){
-
-    let player = playerSelection.toLowerCase();
+    let player = playerSelection; //need to handle user input
 
     const playerWinTemplate = `You win! ${player} beats ${computerSelection}`;
     const playerLoseTemplate = `You lose! ${computerSelection} beats ${playerSelection}`;
     const sameChoiceTemplate = `You and the computer both chose ${playerSelection}!`;
 
-    switch (player) {
-        case 'rock':
-            if(computerSelection == 'paper'){
-                compWinTotal++;
-                return playerLoseTemplate;
-            }else if(computerSelection == 'scissors'){
-                playerWinTotal++;
-                return playerWinTemplate;
-            }else{ //player and computer made same choice
-                tieTotal++;
-                return sameChoiceTemplate;
-            }
-        case 'paper':
-            if(computerSelection == 'scissors'){
-                compWinTotal++;
-                return playerLoseTemplate;
-            }else if(computerSelection == 'rock'){
-                playerWinTotal++;
-                return playerWinTemplate;
-            }else{ //player and computer made same choice
-                tieTotal++;
-                return sameChoiceTemplate;
-            }
-        case 'scissors':
-            if(computerSelection == 'rock'){
-                compWinTotal++;
-                return playerLoseTemplate;
-            }else if(computerSelection == 'paper'){
-                playerWinTotal++;
-                return playerWinTemplate;
-            }else{ //player and computer made same choice
-                tieTotal++;
-                return sameChoiceTemplate;
-            }
-        default:
-            console.log('switch in playRound is not working correcty');
-    }
+    const scoreTemplate = `Human win total: ${playerWinTotal} 
+    Computer win total: ${compWinTotal}
+    Tie total: ${tieTotal}`;
+        
+        if(playerWinTotal == 5) {
+            alert('Congratulations, you beat the computer.')
+
+        }else if(compWinTotal == 5){
+            alert('Computer beat you, too bad.')
+
+        }else{
+ 
+            switch (player) {
+                case 'rock':
+                    if(computerSelection == 'paper'){
+                        ++compWinTotal;
+                        //alert(playerLoseTemplate); 
+                        scoreboard.textContent = scoreTemplate;
+                        resultsContent.textContent = playerLoseTemplate;
+                        resultsContainer.appendChild(resultsContent);
+                        break;
+                    }else if(computerSelection == 'scissors'){
+                        ++playerWinTotal;
+                        //alert(playerWinTemplate);
+                        scoreboard.textContent = scoreTemplate;
+                        resultsContent.textContent = playerWinTemplate;
+                        resultsContainer.appendChild(resultsContent);
+                        break;
+                    }else{ //player and computer made same choice
+                        ++tieTotal;
+                        //alert(sameChoiceTemplate);
+                        scoreboard.textContent = scoreTemplate;
+                        resultsContent.textContent = sameChoiceTemplate;
+                        resultsContainer.appendChild(resultsContent);
+                        break;
+                    }
+                case 'paper':
+                    if(computerSelection == 'scissors'){
+                        ++compWinTotal;
+                        //alert(playerLoseTemplate);
+                        scoreboard.textContent = scoreTemplate;
+                        resultsContent.textContent = playerLoseTemplate;
+                        resultsContainer.appendChild(resultsContent);
+                        break;
+                    }else if(computerSelection == 'rock'){
+                        ++playerWinTotal;
+                        //alert(playerWinTemplate);
+                        scoreboard.textContent = scoreTemplate;
+                        resultsContent.textContent = playerWinTemplate;
+                        resultsContainer.appendChild(resultsContent);
+                        break;
+                    }else{ //player and computer made same choice
+                        ++tieTotal;
+                        //alert(sameChoiceTemplate);
+                        scoreboard.textContent = scoreTemplate;
+                        resultsContent.textContent = sameChoiceTemplate;
+                        resultsContainer.appendChild(resultsContent);
+                        break;
+                    }
+                case 'scissors':
+                    if(computerSelection == 'rock'){
+                        ++compWinTotal;
+                        //alert(playerLoseTemplate);
+                        scoreboard.textContent = scoreTemplate;
+                        resultsContent.textContent = playerLoseTemplate;
+                        resultsContainer.appendChild(resultsContent);
+                        break;
+                    }else if(computerSelection == 'paper'){
+                        ++playerWinTotal;
+                        //alert(playerWinTemplate);
+                        scoreboard.textContent = scoreTemplate;
+                        resultsContent.textContent = playerWinTemplate;
+                        resultsContainer.appendChild(resultsContent);
+                        break;
+                    }else{ //player and computer made same choice
+                        ++tieTotal;
+                        //alert(sameChoiceTemplate);
+                        scoreboard.textContent = scoreTemplate;
+                        resultsContent.textContent = sameChoiceTemplate;
+                        resultsContainer.appendChild(resultsContent);
+                        break;
+                    }
+                default:
+                    console.log('switch in playRound is not working correcty');
+                }
+
+        }
 
 }
 
+
+/*
 function game(){
     for (let i = 0; i < 5; i++) {
         console.log(playRound());
@@ -79,7 +147,9 @@ function game(){
                   );
 
 }
-        
+*/
+
+
 
 
 
